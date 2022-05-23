9c4ca51395332aedcace5dc39c4fcce96ad7f55d
Nand Dalal <nndalal@umich.edu>
2022-05-23 01:25:55 -0400

    Exercise the bazel cache by seeing if it is fast to add a test (#7)

--- sample_module_test.py
+++ sample_module_test.py
@@ -4,9 +4,13 @@ import sample_module
 
 
 class TestSampleModule(unittest.TestCase):
-    def testSampleFunction(self):
+    def testSampleFunctionShape(self):
         result = sample_module.sample_function()
         self.assertEqual(result.shape, (4, 4))
+
+    def testSampleFunctionSum(self):
+        result = sample_module.sample_function()
+        self.assertEqual(result.sum(), 0)
 
 
 if __name__ == "__main__":
